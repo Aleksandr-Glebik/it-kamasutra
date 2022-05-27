6fd19bc525f1faba5a6faaee187c254dfbca56f2
Aleksandr-Glebik <glebikaa@mail.ru>
2022-05-28 00:40:26 +0300

    feat: learn modules: import, export

---
+++ projects/01-first-project/react-1/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <div className="Header">
+        <a href="#">Home</a>
+        <a href="#">New Feed</a>
+        <a href="#">Message</a>
+      </div>
+    )
+  }
+
+  export default Header;
