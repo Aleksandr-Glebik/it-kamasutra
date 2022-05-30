0971917d0f05bf97756ee775d5a4c95e84113bd1
Aleksandr-Glebik <glebikaa@mail.ru>
2022-05-31 00:05:21 +0300

    feat: add markup in dialogs

--- projects/01-first-project/react-1/src/components/Dialogs/Dialogs.jsx
+++ projects/01-first-project/react-1/src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import styles from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom'
 
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={`${styles.dialog} ${styles.active}`}>
+                   <NavLink to='/dialogs/1'>Sasha</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/2'>Pasha</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/3'>Masha</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/4'>Natasha</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/5'>Olga</NavLink>
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi!</div>
+                <div className={styles.message}>Hello, my friend</div>
+                <div className={styles.message}>I am not your friend</div>
+                <div className={styles.message}>Whay</div>
+                <div className={styles.message}>Because you don't know how to react</div>
+                <div className={styles.message}>BUT I AM LEARNING</div>
+                <div className={styles.message}>Good, now you are my friend!!!</div>
+            </div>
         </div>
     )
 }
