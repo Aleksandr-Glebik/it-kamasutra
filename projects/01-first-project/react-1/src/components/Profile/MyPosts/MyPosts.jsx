4d0fc8dec310a0b96c5a59adc01b12fbe5f47184
Aleksandr-Glebik <glebikaa@mail.ru>
2022-06-07 18:41:55 +0300

    feat: add profileReducer, dialogsReducer and optimization state

--- projects/01-first-project/react-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ projects/01-first-project/react-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './MyPosts.module.css'
 import Post from './Post/Post';
-import { addPostActionCreator, updateNewPostText } from '../../../redux/state'
+import { addPostActionCreator, updateNewPostText } from '../../../redux/profile-reducer'
 
 const MyPosts = (props) => {
   let postsElements = props.postsData.map( (post) => {
