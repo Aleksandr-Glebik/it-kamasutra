ea4d0153552828c9cea48a2f15ec3f4a340299f5
Aleksandr-Glebik <glebikaa@mail.ru>
2022-06-09 17:56:00 +0300

    feat: UsersContainer, Users, users.module.css, users-reducer; refactor: App, redux-store

--- projects/01-first-project/react-1/src/App.js
+++ projects/01-first-project/react-1/src/App.js
@@ -8,6 +8,8 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Setting from './components/Setting/Setting';
 import DialogsContainer from './components/Dialogs/DialogsContainer'
+// import Users from './components/Users/Users';
+import UsersContainer from './components/Users/UsersContainer';
 
 const App = (props) => {
   return (
@@ -19,6 +21,7 @@ const App = (props) => {
           <Routes>
             <Route path='/dialogs' element={<DialogsContainer />} />
             <Route path='/profile'element={<Profile />} />
+            <Route path='/users' element={<UsersContainer />} />
             <Route path='/news' element={<News />} />
             <Route path='/music' element={<Music />} />
             <Route path='/setting' element={<Setting />} />
