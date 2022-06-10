4e872de07d78ac3b6560d099291d411f4da5bfa5
Aleksandr-Glebik <glebikaa@mail.ru>
2022-06-10 12:27:40 +0300

    feat: add pagination for page Users

--- projects/01-first-project/react-1/src/redux/users-reducer.js
+++ projects/01-first-project/react-1/src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET-USERS'
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT'
 
 let initialState = {
-    users: []
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1,
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -31,7 +36,19 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS: {
             return {
                 ...state,
-                users: [...state.users, ...action.users]
+                users: action.users
+            }
+        }
+        case SET_CURRENT_PAGE: {
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {
+                ...state,
+                totalUsersCount: action.totalUsersCount
             }
         }
         default:
@@ -57,6 +74,19 @@ export const setUsersActionCreator = (users) => {
     return {
         type: SET_USERS,
         users
+    }
+}
+export const setCurrentPageActionCreator = (currentPage) => {
+    return {
+        type: SET_CURRENT_PAGE,
+        currentPage
+    }
+}
+
+export const setUsersTotalCountActionCreator = (totalUsersCount) => {
+    return {
+        type: SET_TOTAL_USERS_COUNT,
+        totalUsersCount
     }
 }
 
