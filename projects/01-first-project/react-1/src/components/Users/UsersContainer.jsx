c81b66bce717baccb28c736f9ce8d91caa5fc426
Aleksandr-Glebik <glebikaa@mail.ru>
2022-06-10 23:34:01 +0300

    feat: add preloader for psge Users

--- projects/01-first-project/react-1/src/components/Users/UsersContainer.jsx
+++ projects/01-first-project/react-1/src/components/Users/UsersContainer.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { connect } from "react-redux";
-import {followActionCreator, unFollowActionCreator, setUsersActionCreator, setCurrentPageActionCreator, setUsersTotalCountActionCreator} from './../../redux/users-reducer'
-import * as axios from 'axios'
-import Users from "./Users"
+import React from 'react';
+import { connect } from 'react-redux';
+import {followActionCreator, unFollowActionCreator, setUsersActionCreator, setCurrentPageActionCreator, setUsersTotalCountActionCreator, toggleIsFetchingActionCreator} from './../../redux/users-reducer';
+import * as axios from 'axios';
+import Users from './Users';
+import Preloader from '../Common/Preloader/Preloader';
 class UsersContainer extends React.Component {
-    
+
     componentDidMount() {
+        this.props.toggleIsFetching(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false)
             this.props.setUsers(response.data.items)
             this.props.setTotalUsersCount(response.data.totalCount)
         });
@@ -14,20 +17,25 @@ class UsersContainer extends React.Component {
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber)
+        this.props.toggleIsFetching(true)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.toggleIsFetching(false)
             this.props.setUsers(response.data.items)
         });
     }
 
     render() {
-        return <Users totalUsersCount={this.props.totalUsersCount}
+        return <>
+                {this.props.isFetching ? <Preloader /> : null}
+                <Users totalUsersCount={this.props.totalUsersCount}
                       pageSize={this.props.pageSize}
                       currentPage={this.props.currentPage}
                       onPageChanged={this.onPageChanged}
                       users={this.props.users}
                       follow={this.props.follow}
                       unfollow={this.props.unfollow}
-                      />
+                />
+               </>
     }
 }
 
@@ -37,6 +45,7 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
+        isFetching: state.usersPage.isFetching,
     }
 }
 
@@ -56,6 +65,9 @@ let mapDispatchToProps = (dispatch) => {
         },
         setTotalUsersCount: (totalCount) => {
             dispatch(setUsersTotalCountActionCreator(totalCount))
+        },
+        toggleIsFetching: (isFetching) => {
+            dispatch(toggleIsFetchingActionCreator(isFetching))
         }
     }
 }
