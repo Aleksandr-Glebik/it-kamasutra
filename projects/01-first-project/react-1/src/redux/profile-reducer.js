25c4442146dd600b293025d809ad1ce8546cd757
Aleksandr-Glebik <glebikaa@mail.ru>
2022-06-21 17:07:23 +0300

    feat: add thunk: getUserProfile, getAuthUserData

--- projects/01-first-project/react-1/src/redux/profile-reducer.js
+++ projects/01-first-project/react-1/src/redux/profile-reducer.js
@@ -1,3 +1,4 @@
+import { usersAPI } from '../api/api';
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET-USER-PROFILE'
@@ -57,6 +58,12 @@ export const updateNewPostText = (text) => {
 
 export const setUserProfile = (profile) => {
     return {type: SET_USER_PROFILE, profile}
+}
+
+export const getUserProfile = (userId) => (dispatch) => {
+    usersAPI.getProfile(userId).then(response => {
+        dispatch(setUserProfile(response.data))
+    });
 }
 
 export default profileReducer;
