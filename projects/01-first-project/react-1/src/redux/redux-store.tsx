f562f879671d3bdeab5b9937063684cb5ef96dd5
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-10 17:53:27 +0300

    refactor: inferActionsType in redux-store

--- projects/01-first-project/react-1/src/redux/redux-store.tsx
+++ projects/01-first-project/react-1/src/redux/redux-store.tsx
@@ -22,8 +22,10 @@ let rootReducers = combineReducers({
 type RootReducerType = typeof rootReducers
 export type AppStateType = ReturnType<RootReducerType>
 
-type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
-export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesTypes<T>>
+// type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
+// export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesTypes<T>>
+
+export type InferActionsTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U} ? U : never
 
 export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
 
