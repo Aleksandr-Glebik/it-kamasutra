0104c8245ad43bc9192c79a8b776fa0fbd8b2aa7
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-24 02:13:15 +0300

    feat: add heade from ant design in comp header, refactor avatar, button, add useSelect

--- projects/01-first-project/react-1/src/App.tsx
+++ projects/01-first-project/react-1/src/App.tsx
@@ -2,19 +2,18 @@ import React from 'react'
 import './App.css'
 import 'antd/dist/antd.css'
 import {BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import HeaderContainer from './components/Header/HeaderContainer.tsx'
-import Nav from './components/Nav/Nav.tsx'
 import UsersPage from './components/Users/UsersContainer.tsx'
 import LoginPage from './components/Login/Login.tsx'
 import { connect } from 'react-redux'
 import {initializeApp} from './redux/app-reducer.ts'
 import Preloader from './components/Common/Preloader/Preloader.tsx'
 import {AppStateType} from './redux/redux-store.tsx'
+import  Header  from './components/Header/Header.tsx'
 
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons'
 import type { MenuProps } from 'antd'
-import { Breadcrumb, Layout, Menu } from 'antd'
-const { Header, Content, Footer, Sider } = Layout
+import { Layout, Menu } from 'antd'
+const {  Content, Footer, Sider } = Layout
 
 const ProfileContainer = React.lazy( () => import('./components/Profile/ProfileContainer.tsx'))
 const DialogsContainer = React.lazy( () => import('./components/Dialogs/DialogsContainer.tsx'))
@@ -27,7 +26,7 @@ type DispatchPropsType = {
   initializeApp: () => void
 }
 
-const menuItems = [
+export const menuItems = [
   {
     key: 'My Profile',
     icon: <UserOutlined />,
@@ -45,10 +44,11 @@ const menuItems = [
   },
 ]
 
-const menuItems1: MenuProps['items'] = menuItems.map(item => ({
+/* const menuItems1: MenuProps['items'] = menuItems.map(item => ({
   key: item.key,
   label: item.label,
-}));
+})); */
+export const menuItems1: MenuProps['items'] = menuItems.filter(item => item.key === 'Developers')
 
 class App extends React.Component<MapPropsType & DispatchPropsType> {
   componentDidMount() {
@@ -60,29 +60,15 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
       return <Preloader />
     }
 
-    return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Layout>
-    <Header className="header">
-      <div className="logo" />
-      <Menu theme="dark"
-            mode="horizontal"
-            // defaultSelectedKeys={['2']}
-            items={menuItems1}
-      />
-    </Header>
+return (
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Layout>
+    <Header />
     <Content style={{ padding: '0 50px' }}>
-      {/* <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb> */}
       <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
         <Sider className="site-layout-background" width={200}>
           <Menu
             mode="inline"
-            // defaultSelectedKeys={['1']}
-            // defaultOpenKeys={['sub1']}
             style={{ height: '100%' }}
             items={menuItems}
           />
@@ -125,50 +111,9 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
         </Content>
       </Layout>
     </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    <Footer style={{ textAlign: 'center' }}>Samurai Social Network ©2020 Created by Aleksandr Glebik in 2022</Footer>
   </Layout>
-        {/* <div className='app-wrapper'>
-          <HeaderContainer />
-          <Nav />
-          <div className='app-wrapper-content'>
-            <Routes>
-              <Route path='/dialogs/*' element={
-                <React.Suspense fallback={<Preloader />}>
-                  <DialogsContainer />
-                </React.Suspense>
-              } />
-              <Route path='/profile/:userId' element={
-                <React.Suspense fallback={<Preloader />}>
-                  <ProfileContainer />
-                </React.Suspense>
-              } />
-              <Route path='/profile/*' element={
-                <React.Suspense fallback={<Preloader />}>
-                  <ProfileContainer />
-                </React.Suspense>
-              } />
-              <Route path='/users/*' element={<UsersPage pageTitle={'Samurai'} />} />
-              <Route path='/news/*' element={
-                <React.Suspense fallback={<Preloader />}>
-                  <News />
-                </React.Suspense>
-              } />
-              <Route path='/music/*' element={
-                <React.Suspense fallback={<Preloader />}>
-                  <Music />
-                </React.Suspense>
-              } />
-              <Route path='/setting/*' element={
-                <React.Suspense fallback={<Preloader />}>
-                  <Setting />
-                </React.Suspense>
-              } />
-              <Route path='/login/*' element={<LoginPage />} />
-            </Routes>
-          </div>
-        </div> */}
-
-              </BrowserRouter>
+</BrowserRouter>
     )
   }
 }
--- projects/01-first-project/react-1/src/components/Header/Header.tsx
+++ projects/01-first-project/react-1/src/components/Header/Header.tsx
@@ -1,27 +1,51 @@
 import React from 'react'
-import styles from './Header.module.css'
-import { NavLink } from 'react-router-dom'
+import { Layout, Menu, Avatar, Row, Col, Button } from 'antd'
+import { menuItems1 } from '../../App.tsx'
+import { UserOutlined } from '@ant-design/icons'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectIsAuth, selectCurrentUserLogin } from '../../redux/auth-selectors.ts'
+import {logout} from '../../redux/auth-reducer.ts'
+import { Link } from 'react-router-dom'
 
-export type MapPropsType = {
-  isAuth: boolean
-  login: string | null
-}
-export type DispatchPropsType = {
-  logout: () => void
-}
+export type PropsType = {}
 
-const Header: React.FC<MapPropsType & DispatchPropsType> = (props) => {
+const Header: React.FC<PropsType> = (props) => {
+    const { Header } = Layout
+    const isAuth = useSelector(selectIsAuth)
+    const login = useSelector(selectCurrentUserLogin)
+
+    const dispatch = useDispatch()
+    const logoutCallBack = () => {
+      dispatch(logout())
+    }
+
     return (
-      <header className={styles.header}>
-        <img src="https://image.shutterstock.com/image-vector/location-icon-vector-simple-sign-600w-1428674642.jpg" alt="logo" className={styles.img}></img>
-
-        <div className={styles.loginBlock}>
-          {props.isAuth
-            ? <div>{props.login} - <button onClick={props.logout}>Logout</button></div>
-            : <NavLink to={'/login'}>Login</NavLink>}
-        </div>
-      </header>
+      <Header className="header">
+      <Row>
+        <Col span={18}>
+          <Menu theme="dark"
+                mode="horizontal"
+                items={menuItems1}
+          />
+        </Col>
+          {isAuth
+            ? <>
+              <Col span={2}>
+                <Avatar title={login} style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+              </Col>
+              <Col span={4}>
+                <Button onClick={logoutCallBack}>Logout</Button>
+              </Col>
+              </>
+            : <Col span={6}>
+                <Button>
+                  <Link to={'/login'}>Login</Link>
+                </Button>
+              </Col>
+          }
+      </Row>
+    </Header>
     )
 }
 
-export default Header;
+export default Header
