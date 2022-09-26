295b67f94ba313fff5246fb41e8e65c83041576b
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-27 00:09:09 +0300

    feat: add useState and useEffect for Searcher, event click for List.Item

--- projects/01-first-project/react-1/src/pages/Search/SearchPage.tsx
+++ projects/01-first-project/react-1/src/pages/Search/SearchPage.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import { Button, Input, List, Card } from 'antd';
+import React, { useEffect, useState } from "react"
+import { Button, Input, List, Card } from 'antd'
+import s from './SearchPage.module.css'
 
 const SearchPage: React.FC = () => {
     return (
@@ -12,9 +13,18 @@ const SearchPage: React.FC = () => {
 const Searcher: React.FC = () => {
 
     const data = ['Sasha', 'Pasha']
+    const [selectedUser, setSelectedUser] = useState<string | null>(null)
+
+    useEffect( () => {
+        console.log('useeffect');
+
+        if (selectedUser) {
+            document.title = selectedUser
+        }
+    }, [selectedUser])
 
     return (
-        <div style={{display: 'flex',}}>
+        <div className={s.container}>
             <div style={{ width: 200, marginRight: 25 }}>
                 <Input placeholder="Search" />
                 <Button>Find</Button>
@@ -22,15 +32,24 @@ const Searcher: React.FC = () => {
                     size="small"
                     bordered
                     dataSource={data}
-                    renderItem={item => <List.Item>{item}</List.Item>}
+                    renderItem={item => <List.Item
+                        className={selectedUser === item
+                            ? s.selected
+                            : ''
+                        }
+                        onClick={() => {
+                            setSelectedUser(item)
+                            // document.title = item
+                        }}
+                    >{item}</List.Item>}
                 />
             </div>
             <div className="site-card-border-less-wrapper">
-            <Card title="Username" bordered={false} style={{ width: 200 }}>
-                <p>Details 1</p>
-                <p>Details 2</p>
-                <p>Details 3</p>
-            </Card>
+                <Card title="Username" bordered={false} style={{ width: 200 }}>
+                    <p>Details 1</p>
+                    <p>Details 2</p>
+                    <p>Details 3</p>
+                </Card>
             </div>
         </div>
     )
