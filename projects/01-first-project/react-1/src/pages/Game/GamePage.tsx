78a3331a22acae149b5718ca1735c575c793766a
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-26 19:36:38 +0300

    feat: add one useState for two counters - initialState obj

--- projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
+++ projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
@@ -12,31 +12,47 @@ const GamePage: React.FC = () => {
 const Game: React.FC = () => {
     console.log('component rendering');
 
-    let [player1Counter, setPlayer1Counter] = useState(0)
-    let [player2Counter, setPlayer2Counter] = useState(0)
+    let [counter, setCounter] = useState({
+        c1: 0,
+        c2: 0
+    })
 
     return (
         <div>
             <div>
                 <div>Иван Иванович</div>
-                <div>{player1Counter}</div>
-                <Button onClick={() => setPlayer1Counter(prev => prev + 1)}>+</Button>
+                <div>{counter.c1}</div>
+                <Button onClick={ () => setCounter(actual => {
+                    return {
+                        ...actual, c1: actual.c1 + 1
+                    }
+                })}>+</Button>
             </div>
             <hr />
             <div>
                 <div>Петр Петрович</div>
-                <div>{player2Counter}</div>
-                <Button onClick={() => setPlayer2Counter(prev => prev + 1)}>+</Button>
+                <div>{counter.c2}</div>
+                <Button onClick={ () => setCounter(actual => {
+                    return {
+                        ...actual, c2: actual.c2 + 1
+                    }
+                })}>+</Button>
             </div>
             <hr />
-            <Button onClick={() => {
-                setPlayer1Counter(prev => prev - 1)
-                setPlayer2Counter(prev => prev - 1)
-            }}>-</Button>
-            <Button onClick={() => {
-                setPlayer1Counter(0)
-                setPlayer2Counter(0)
-            }}>reset game</Button>
+            <Button onClick={ () => setCounter(actual => {
+                    return {
+                        ...actual,
+                        c1: actual.c1 - 1,
+                        c2: actual.c2 - 1
+                    }
+                })}>-</Button>
+            <Button onClick={ () => setCounter(actual => {
+                    return {
+                        ...actual,
+                        c1: 0,
+                        c2: 0
+                    }
+                })}>reset game</Button>
         </div>
     )
 }
