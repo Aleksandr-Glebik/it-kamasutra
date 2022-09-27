b48d73f23937b0879c08bdac84aac53fed65a302
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-27 14:28:34 +0300

    feat: add useEffect - get axios inform about user

--- projects/01-first-project/react-1/src/pages/Search/SearchPage.tsx
+++ projects/01-first-project/react-1/src/pages/Search/SearchPage.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react"
-import { Button, Input, List, Card } from 'antd'
+import { Button, Input, List, Card, Image } from 'antd'
 import s from './SearchPage.module.css'
 import axios from "axios"
 
-//https://api.github.com/search/users?q=it-kamasutra
+// url search users = https://api.github.com/search/users?q=${it-kamasutra}
+// url search user = https://api.github.com/users/${it-kamasutra}
 
 const SearchPage: React.FC = () => {
     return (
@@ -22,12 +23,21 @@ type SearchResultType = {
     items: SearchUserType[]
 }
 
+type UserType = {
+    login: string
+    id: number
+    avatar_url: string
+    public_repos: number
+    followers: number
+}
+
 const Searcher: React.FC = () => {
 
     const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
     const [users, setUsers] = useState<SearchUserType[]>([])
     const [tempSearch, setTempSearch] = useState('it-kamasutra')
     const [searchTerm, setSearchTerm] = useState('it-kamasutra')
+    const [userDetails, setUserDetails] = useState<UserType | null>(null)
 
     useEffect( () => {
         // console.log('sync tab title')
@@ -44,6 +54,17 @@ const Searcher: React.FC = () => {
                 setUsers(res.data.items)
             })
     }, [searchTerm])
+
+    useEffect( () => {
+        console.log('sync user details')
+        if (!!selectedUser) {
+            axios
+            .get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
+            .then(res => {
+                setUserDetails(res.data)
+            })
+        }
+    }, [selectedUser])
 
     return (
         <div className={s.container}>
@@ -76,11 +97,23 @@ const Searcher: React.FC = () => {
                 />
             </div>
             <div className="site-card-border-less-wrapper">
-                <Card title="Username" bordered={false} style={{ width: 200 }}>
-                    <p>Details 1</p>
-                    <p>Details 2</p>
-                    <p>Details 3</p>
-                </Card>
+                {userDetails &&
+                <Card title={userDetails?.login}
+                      bordered={false}
+                      style={{ width: 200 }}
+                      cover={
+                        <Image
+                          width={200}
+                          alt="user-photo"
+                          src={userDetails?.avatar_url}
+                        />
+                      }
+                >
+                    <p>login: {userDetails?.login}</p>
+                    <p>ID: {userDetails?.id}</p>
+                    <p>Followers: {userDetails?.followers}</p>
+                    <p>Public repositories: {userDetails?.public_repos}</p>
+                </Card>}
             </div>
         </div>
     )
