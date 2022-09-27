6dcbbc6efd95dc65a9218b0bc0543091bcdf99da
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-28 01:41:47 +0300

    feat: add markup: card and image for GamePage component

--- projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
+++ projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd"
+import { Button, Card, Image } from "antd"
 import React, { useState } from "react"
 
 const GamePage: React.FC = () => {
@@ -18,41 +18,55 @@ const Game: React.FC = () => {
     })
 
     return (
-        <div>
-            <div>
-                <div>Иван Иванович</div>
-                <div>{counter.c1}</div>
+        <div style={{width: 900, display: 'flex', justifyContent: 'space-between'}}>
+            <Card title={'Иван Иванович'}
+                  hoverable
+                  style={{ width: 250 }}
+                  cover={
+                  <Image alt="photo"
+                       style={{ width: 250 }}
+                       src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                  }
+            >
+                <h2>Счет: {counter.c1}</h2>
                 <Button onClick={ () => setCounter(actual => {
                     return {
                         ...actual, c1: actual.c1 + 1
                     }
                 })}>+</Button>
-            </div>
-            <hr />
-            <div>
-                <div>Петр Петрович</div>
-                <div>{counter.c2}</div>
+            </Card>
+            <Card title={'Петр Петрович'}
+                  hoverable
+                  style={{ width: 250 }}
+                  cover={
+                  <Image alt="photo"
+                       style={{ width: 250 }}
+                       src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+                  }
+            >
+                <h2>Счет: {counter.c2}</h2>
                 <Button onClick={ () => setCounter(actual => {
                     return {
                         ...actual, c2: actual.c2 + 1
                     }
                 })}>+</Button>
-            </div>
-            <hr />
-            <Button onClick={ () => setCounter(actual => {
-                    return {
-                        ...actual,
-                        c1: actual.c1 - 1,
-                        c2: actual.c2 - 1
-                    }
+            </Card>
+            <div>
+                <Button onClick={ () => setCounter(actual => {
+                        return {
+                            ...actual,
+                            c1: actual.c1 - 1,
+                            c2: actual.c2 - 1
+                        }
                 })}>-</Button>
-            <Button onClick={ () => setCounter(actual => {
-                    return {
-                        ...actual,
-                        c1: 0,
-                        c2: 0
-                    }
+                <Button onClick={ () => setCounter(actual => {
+                        return {
+                            ...actual,
+                            c1: 0,
+                            c2: 0
+                        }
                 })}>reset game</Button>
+            </div>
         </div>
     )
 }
