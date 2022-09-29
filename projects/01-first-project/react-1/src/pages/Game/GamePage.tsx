4cf6b3f4e81b4b2587842ba3f957e6b61be345ef
Aleksandr-Glebik <glebikaa@mail.ru>
2022-09-29 19:14:59 +0300

    feat: add markup and style for GamePage, game and Setup section

--- projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
+++ projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
@@ -1,7 +1,9 @@
-import { Button } from "antd"
 import React, { useState } from "react"
+import { Button } from "antd"
 import DrawerRules from './DrawerRules.tsx'
 import PersonCard from './PersonCard.tsx'
+import GameTimer from './GameTimer.tsx'
+import styles from './GamePage.module.css'
 
 const GamePage: React.FC = () => {
     return (
@@ -16,63 +18,64 @@ const initialImageSrc = 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.
 const Game: React.FC = () => {
     console.log('component rendering');
 
+    let [setupSectionForm, setSetupSectionForm] = useState(false)
+    let [setupSectionButton, setSetupSectionButton] = useState(false)
+
     let [counter, setCounter] = useState({
         c1: 0,
         c2: 0
     })
 
     return (
-        <div style={{width: 900, display: 'flex', justifyContent: 'space-between'}}>
-            {/* <Card title={'Иван Иванович'}
-                  hoverable
-                  style={{ width: 250 }}
-                  cover={
-                  <Image alt="photo"
-                       style={{ width: 250 }}
-                       src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
-                  }
-            >
-                <h2>Счет: {counter.c1}</h2>
-            </Card> */}
-            <PersonCard cardTitle={'str1'} imageSrc={initialImageSrc} count={counter.c1} />
-            <PersonCard cardTitle={'str2'} imageSrc={initialImageSrc} count={counter.c2} />
-            {/* <Card title={'Петр Петрович'}
-                  hoverable
-                  style={{ width: 250 }}
-                  cover={
-                  <Image alt="photo"
-                       style={{ width: 250 }}
-                       src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
-                  }
-            >
-                <h2>Счет: {counter.c2}</h2>
-            </Card> */}
-            <div>
-                <Button onClick={ () => setCounter(actual => {
+        <div className={styles.container}>
+            <div className={styles.gameSection}>
+                <PersonCard cardTitle={'str1'} imageSrc={initialImageSrc} count={counter.c1} />
+                <GameTimer className={styles.gameTimer}/>
+                <PersonCard cardTitle={'str2'} imageSrc={initialImageSrc} count={counter.c2} />
+            </div>
+            <div className={styles.setupSection}>
+                <div className={styles.setupSectionNav}>
+                    <Button danger
+                            onClick={() => setSetupSectionForm(prev => !prev)}
+                    >Настройка</Button>
+                    <Button type="default"
+                            onClick={() => setSetupSectionButton(prev => !prev)}
+                    >Играть</Button>
+                    <DrawerRules />
+                </div>
+                {setupSectionForm &&  <div className={styles.setupSectionForm}>
+                    Секция где будут импуты для установки имен и фото
+                </div>}
+                {setupSectionButton && <div className={styles.setupSectionButton}>
+                    <div>
+                        <Button onClick={ () => setCounter(actual => {
                         return {
-                            ...actual,
-                            c1: actual.c1 - 1,
-                            c2: actual.c2 - 1
+                            ...actual, c1: actual.c1 + 1
                         }
-                })}>-</Button>
-                <Button onClick={ () => setCounter(actual => {
-                        return {
-                            ...actual,
-                            c1: 0,
-                            c2: 0
-                        }
-                })}>reset game</Button>
-                <DrawerRules />
-                <Button onClick={ () => setCounter(actual => {
-                    return {
-                        ...actual, c1: actual.c1 + 1
-                    }
-                })}>+ очко 1 игроку</Button>
-                <Button onClick={ () => setCounter(actual => {
-                    return {
-                        ...actual, c2: actual.c2 + 1
-                    }
-                })}>+ очко 2 игроку</Button>
+                        })}>+ очко 1 игроку</Button>
+                        <Button onClick={ () => setCounter(actual => {
+                            return {
+                                ...actual, c2: actual.c2 + 1
+                            }
+                        })}>+ очко 2 игроку</Button>
+                    </div>
+                    <div>
+                        <Button onClick={ () => setCounter(actual => {
+                            return {
+                                ...actual,
+                                c1: actual.c1 - 1,
+                                c2: actual.c2 - 1
+                            }
+                        })}>-</Button>
+                        <Button onClick={ () => setCounter(actual => {
+                                return {
+                                    ...actual,
+                                    c1: 0,
+                                    c2: 0
+                                }
+                        })}>reset game</Button>
+                    </div>
+                </div>}
             </div>
         </div>
     )
