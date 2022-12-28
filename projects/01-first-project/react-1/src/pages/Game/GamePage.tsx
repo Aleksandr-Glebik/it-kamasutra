61acdcfdef4a8b0f5459547a78a3e0e9192237e3
Aleksandr-Glebik <glebikaa@mail.ru>
2022-12-28 13:32:36 +0300

    feat: add setGender, changes photo and icon in PersonCard

--- projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
+++ projects/01-first-project/react-1/src/pages/Game/GamePage.tsx
@@ -5,6 +5,7 @@ import PersonCard from './PersonCard.tsx'
 import GameTimer from './GameTimer.tsx'
 import CastomFormCard from './CastomFormCard.tsx'
 import styles from './GamePage.module.css'
+// import { values } from "lodash"
 
 const GamePage: React.FC = () => {
     return (
@@ -14,7 +15,9 @@ const GamePage: React.FC = () => {
     )
 }
 
-const initialImageSrc = 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'
+const initialImageSrc = 'https://source.unsplash.com/user/jakehills/z0gDv24X3uQ/1920x1280'
+const imageSrcMan = 'https://source.unsplash.com/user/timbog80/4uojMEdcwI8/1920x1280'
+const imageSrcWoman = 'https://source.unsplash.com/user/denysnevozhai/z0nVqfrOqWA/1920x1280'
 
 const Game: React.FC = () => {
     console.log('component rendering');
@@ -25,6 +28,36 @@ const Game: React.FC = () => {
     let [nameOne, setNameOne] = useState('Name 1')
     let [nameTwo, setNameTwo] = useState('Name 2')
 
+    let [genderOne, setGenderOne] = useState('')
+    let [genderTwo, setGenderTwo] = useState('')
+
+    let [imageSrcOne, setImageSrcOne] = useState(initialImageSrc)
+    let [imageSrcTwo, setImageSrcTwo] = useState(initialImageSrc)
+
+    function handleSubmitOne({name, gender}) {
+        setNameOne(name)
+        setGenderOne(gender)
+        if (gender === 'man') {
+            setImageSrcOne(imageSrcMan)
+        } else if (gender === 'woman') {
+            setImageSrcOne(imageSrcWoman)
+        } else {
+            setImageSrcOne(initialImageSrc)
+        }
+    }
+
+    function handleSubmitTwo({name, gender}) {
+        setNameTwo(name)
+        setGenderTwo(gender)
+        if (gender === 'man') {
+            setImageSrcTwo(imageSrcMan)
+        } else if (gender === 'woman') {
+            setImageSrcTwo(imageSrcWoman)
+        } else {
+            setImageSrcTwo(initialImageSrc)
+        }
+    }
+
 
     let [counter, setCounter] = useState({
         c1: 0,
@@ -34,9 +67,9 @@ const Game: React.FC = () => {
     return (
         <div className={styles.container}>
             <div className={styles.gameSection}>
-                <PersonCard cardTitle={nameOne} imageSrc={initialImageSrc} count={counter.c1} />
+                <PersonCard cardTitle={nameOne} imageSrc={imageSrcOne} count={counter.c1} gender={genderOne}/>
                 <GameTimer className={styles.gameTimer}/>
-                <PersonCard cardTitle={nameTwo} imageSrc={initialImageSrc} count={counter.c2} />
+                <PersonCard cardTitle={nameTwo} imageSrc={imageSrcTwo} count={counter.c2} gender={genderTwo}/>
             </div>
             <div className={styles.setupSection}>
                 <div className={styles.setupSectionNav}>
@@ -49,8 +82,8 @@ const Game: React.FC = () => {
                     <DrawerRules />
                 </div>
                 {setupSectionForm &&  <div className={styles.setupSectionForm}>
-                    <CastomFormCard cardTitle={'Данные Первого игрока'} name={nameOne} onSubmit={setNameOne} />
-                    <CastomFormCard cardTitle={'Данные Второго игрока'} name={nameTwo} onSubmit={setNameTwo} />
+                    <CastomFormCard cardTitle={'Данные Первого игрока'} name={nameOne} onSubmit={handleSubmitOne} gender={genderOne} />
+                    <CastomFormCard cardTitle={'Данные Второго игрока'} name={nameTwo} onSubmit={handleSubmitTwo} gender={genderTwo} />
                 </div>}
                 {setupSectionButton && <div className={styles.setupSectionButton}>
                     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
@@ -82,7 +115,7 @@ const Game: React.FC = () => {
                                 })}>- очко 2 игроку</Button>
                             </div>
                         </div>
-                        <div style={{justifySelf: 'flex-end',     alignSelf: 'center', padding: 20}}>
+                        <div style={{justifySelf: 'flex-end', alignSelf: 'center', padding: 20}}>
                             <Button>Старт</Button>
                             <Button>Остановить время</Button>
                             <Button onClick={ () => setCounter(actual => {
